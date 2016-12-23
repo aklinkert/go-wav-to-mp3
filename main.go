571e7f2e7305fd72975a509fbd5c3044ac3939b2
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2016-12-23 01:20:57 +0100

    Added wav-to-mp3 util

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+
+	"os"
+	"sync"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	var dir string
+	if len(os.Args) == 2 {
+		dir = os.Args[1]
+	} else {
+		dir = "."
+	}
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+
+	var filesToConvert []string
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		if !strings.Contains(file.Name(), ".wav") {
+			fmt.Printf("File '%s' is not a wav file. \n", file.Name())
+			continue
+		}
+
+		if _, err := os.Stat(file.Name()); err != nil {
+			fmt.Printf("File '%s' does not exist. \n", file.Name())
+		}
+
+		if _, err := os.Stat(strings.Replace(file.Name(), ".wav", ".mp3", 1)); err == nil {
+			fmt.Printf("There is already a .mp3 file for %s \n", file.Name())
+		}
+
+		filesToConvert = append(filesToConvert, file.Name())
+		fmt.Printf("Added '%s' to queue. \n", file.Name())
+	}
+
+	if len(filesToConvert) == 0 {
+		fmt.Println("Nothing to do.")
+		return
+	}
+
+	fmt.Println("")
+	fmt.Println("###########################################################")
+	fmt.Println("###########################################################")
+	fmt.Println("###########################################################")
+	fmt.Println("")
+
+	var wg sync.WaitGroup
+	wg.Add(len(filesToConvert))
+	for _, p := range filesToConvert {
+		go convert(p, &wg)
+	}
+
+	wg.Wait()
+
+	fmt.Println("")
+	fmt.Println("###########################################################")
+	fmt.Println("")
+
+	fmt.Println("All files converted.")
+}
+
+func convert(path string, wg *sync.WaitGroup) {
+	cmd := exec.Command("lame", "--silent", "-h", "-V2", path)
+
+	if output, err := cmd.CombinedOutput(); err != nil {
+		fmt.Println("Build:", err)
+		fmt.Println("Build:", string(output))
+		fmt.Println("Aborting.")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Done converting '%s' \n", path)
+	wg.Done()
+}
